29758062e8e13d8d040f5c4997ace014a4b90263
nice <you@example.com>
2019-10-13 09:19:31 +0200

    added a form to create an account

---
+++ app/src/main/java/com/example/firstip/createAccount.java
@@ -0,0 +1,19 @@
+package com.example.firstip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class createAccount extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+
+    }
+}
